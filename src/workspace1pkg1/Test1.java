17f7018bc3ad4ef58b22c1be8c578ded94e5d029
Sneha Prabhu <snehaprabhu164@gmail.com>
2025-11-15 22:25:12 +0530

    siddhi push code 3rd time

--- src/workspace1pkg1/Test1.java
+++ src/workspace1pkg1/Test1.java
@@ -1,28 +1,36 @@
 package workspace1pkg1;
 
 public class Test1 {
-<<<<<<< HEAD
+
 	public void siddhiTest() {
 		System.out.println("this is my workspace1");
 	}
-	public static void main(String[]args) {
-System.out.println("this is my workspace1");
-=======
+
+	public static void main(String[] args) {
+		System.out.println("this is my workspace1");
+	}
+
 	public void test1() {
 		System.out.println("this is my test1");
 	}
+
 	public void test2() {
 		System.out.println("this is my test2");
 	}
+
 	public void test3() {
 		System.out.println("this is my test3");
 	}
+
 	public void test4() {
 		System.out.println("this is my test3");
 	}
-	public static void main(String[]args) {
-System.out.println("this is my workspace1");
 
->>>>>>> 4b75f42ab89269ad8696c47a2df01902bbc0e5f2
-}
+	public void siddhiTest1() {
+		System.out.println("this is my workspace1");
+	}
+	
+	public void siddhiTest2() {
+		System.out.println("this is my workspace1");
+	}
 }
