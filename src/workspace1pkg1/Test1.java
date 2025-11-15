4fec2b8e7c17ba4bc362de90f97a0d0c8872d296
Sneha Prabhu <snehaprabhu164@gmail.com>
2025-11-15 22:15:50 +0530

    siddhi added the chnage

--- src/workspace1pkg1/Test1.java
+++ src/workspace1pkg1/Test1.java
@@ -1,6 +1,9 @@
 package workspace1pkg1;
 
 public class Test1 {
+	public void siddhiTest() {
+		System.out.println("this is my workspace1");
+	}
 	public static void main(String[]args) {
 System.out.println("this is my workspace1");
 }
