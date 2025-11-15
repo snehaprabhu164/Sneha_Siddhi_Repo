f829d74c7df45e8e879ef0c073aead4cf7a16638
Sneha Prabhu <snehaprabhu164@gmail.com>
2025-11-15 21:52:44 +0530

    Sneha Fist Time Push The Code

---
+++ src/workspace1pkg1/Test1.java
@@ -0,0 +1,17 @@
+package workspace1pkg1;
+
+public class Test1 {
+	public void test1() {
+		System.out.println("this is my test1");
+	}
+	public void test2() {
+		System.out.println("this is my test2");
+	}
+	public void test3() {
+		System.out.println("this is my test3");
+	}
+	public static void main(String[]args) {
+System.out.println("this is my workspace1");
+
+}
+}
